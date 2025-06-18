f51120dc2f6ce9e3e53d5de0704680655f1958bc
cankadirr <cankadirrdb@gmail.com>
2025-06-18 22:42:08 +0300

    Update sanity.config.ts

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -5,12 +5,9 @@ import schema from './schemas/schema'
 export default defineConfig({
   name: 'default',
   title: 'pung-clean',
-
   projectId: '13f1s0mc',
   dataset: 'production',
-
   plugins: [deskTool()],
-
   schema: {
     types: schema.types,
   },
